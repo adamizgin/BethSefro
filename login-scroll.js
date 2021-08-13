9be3d20b7bf75f9bc4c231e59c1a6f6723e1e074
test konto <testkonto@AppleProSchool.local>
2021-08-13 14:56:08 +0200

    Beth Sefro

---
+++ login-scroll.js
@@ -0,0 +1,35 @@
+$(window).on('scroll', function(e) {
+	arhesh = $(this).scrollTop();
+	SH = $('#mrabco1').height() / 10;
+	if (arhesh >= SH) {
+		$('#risho').css({
+			'background': '#fff',
+			'box-shadow': '0 0 10px 0 #0003'
+		});
+		$('#risho *').css({
+			'color': '#000'
+		});
+		$('#logo-heworo').css({
+			'-webkit-filter': 'invert(100%)',
+			'filter': 'invert(100%)'
+		});
+		$('.hamLine').css({
+			'background': '#000'
+		});
+	} else {
+		$('#risho').css({
+			'background': 'transparent',
+			'box-shadow': 'none'
+		});
+		$('#risho *').css({
+			'color': '#fff'
+		});
+		$('#logo-heworo').css({
+			'-webkit-filter': 'none',
+			'filter': 'none'
+		});
+		$('.hamLine').css({
+			'background': '#fff'
+		});
+	}
+});
---
+++ script.js
@@ -0,0 +1,84 @@
+$(document).ready(function() {
+	var rellax;
+	if ($('.relax')) {
+		relax = new Rellax('.rellax');
+	}
+	setTimeout(function() {
+		$('#loading-page').fadeOut();
+	}, 500);
+
+	$('.hvus').click(function() {
+		$hano = $(this);
+		$(this).find('.hvus-animation').show();
+		$(this).find('.hvus-animation').css({
+			'height': '0',
+			'transition': 'none'
+		});
+		setTimeout(function() {
+			$hano.find('.hvus-animation').css({
+				'transition': 'height 250ms ease-in-out'
+			});
+			setTimeout(function() {
+				$hano.find('.hvus-animation').css({
+					'height': '100%'
+				});
+				setTimeout(function() {
+					$hano.find('.hvus-animation').fadeOut();
+				}, 450);
+			});
+		});
+	});
+
+	$('#login-uName').focus(function() {
+		$('#uName-border').css({
+			'width': '100%'
+		});
+	});
+	$('#login-uName').blur(function() {
+		$('#uName-border').css({
+			'width': '0'
+		});
+	});
+	$('#login-pwd').focus(function() {
+		$('#pwd-border').css({
+			'width': '100%'
+		});
+	});
+	$('#login-pwd').blur(function() {
+		$('#pwd-border').css({
+			'width': '0'
+		});
+	});
+
+	$('.login-form button').append('<span class="ripple"></span>');
+	$('.login-form button').click(function(e) {
+		let top = $(window).scrollTop() - $(this).offset().top + e.clientY;
+		let left = $(window).scrollLeft() - $(this).offset().left + e.clientX;
+		$(this).children('span').css({
+			"left": left,
+			"top": top
+		});
+		$(this).children('span').addClass('activeRipple');
+		setTimeout(function() {
+			$('.ripple').removeClass('activeRipple');
+		}, 500);
+	});
+
+	$('#login-hvus').click(function() {
+		window.open('login-page.html', '_self');
+	});
+	$('#home-hvus').click(function() {
+		window.open('Beth%20Sefro.html', '_self');
+	});
+
+	// Login system
+	var name = $('#login-uName');
+	var pwd = $('#login-pwd');
+	var loginBtn = $('#login-button');
+	var loginError = $('#login-error');
+	loginBtn.click(function() {
+		if ($('#login-uName').html()) {
+			alert();
+		}
+	});
+});
